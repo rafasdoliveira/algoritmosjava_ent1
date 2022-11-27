a3f29bda3a354263d077b6471007826277914de3
Rafael Oliveira <rafasdoliveira@outlook.com>
2022-11-27 01:07:18 -0300

    primo done

--- Entregavel1/src/algortimosjava/primo.java
+++ Entregavel1/src/algortimosjava/primo.java
@@ -5,6 +5,23 @@ import java.util.Scanner;
 public class primo {
 
 	public static void main(String[] args) {
-		Scanner num = new Scanner(System.in);
+		try (Scanner n = new Scanner(System.in)) {
+		int num;
+		int numDivisores=0;
+		
+		System.out.print("Informe um numero");
+		num = n.nextInt();
+		
+		for(int i=1; i<=num; i++) {
+			if(num%i==0) {
+				numDivisores++;
+				}
+			}
+		if(numDivisores==2) {
+			System.out.print("O numero eh primo");
+			} else {
+			System.out.print("O numero nao eh primo");
+			}
+		}
 	}
 }
