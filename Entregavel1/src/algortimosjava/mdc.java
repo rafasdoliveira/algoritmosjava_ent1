0ed2c55630193568c5c8902917c3b2bc6f8e3eeb
Rafael Oliveira <rafasdoliveira@outlook.com>
2022-11-27 19:39:18 -0300

    mdc done

--- Entregavel1/src/algortimosjava/mdc.java
+++ Entregavel1/src/algortimosjava/mdc.java
@@ -1,5 +1,35 @@
 package algortimosjava;
 
-public class mdc {
+import java.util.Scanner;
 
+public class mdc {
+	
+	public static void main(String[] args) {
+		
+		try(Scanner n = new Scanner(System.in)) {
+			int num1=0;
+			System.out.print("Digite o primeiro numero");
+			num1 = n.nextInt();
+			int num2=0;
+			System.out.print("Digite o segundo numero");
+			num2 = n.nextInt();
+			
+			int maior, menor;
+			if(num1>num2) {
+				maior= num1;
+				menor= num2;
+			} else {
+				maior= num2;
+				menor= num1;
+			}
+			int aux=0;
+			while((maior%menor)!=0) {
+				aux= menor;
+				menor= (maior%menor);
+				aux= maior;
+			}
+			System.out.print("Maximo Divisor Comum de ("+ num1 + " , " + num2 + ") = " +
+				    menor);
+		}
+	}
 }
