46c8499032558387dcdd0b0c7d1e485f0c175e52
Rafael Oliveira <rafasdoliveira@outlook.com>
2022-11-26 22:39:44 -0300

    First Commit - Somatorio

---
+++ Entregavel1/src/algortimosjava/somatorio.java
@@ -0,0 +1,10 @@
+package algortimosjava;
+
+public class somatorio {
+	
+	public static void main(String[] args) {
+		
+		System.out.print("Ola, mundo!");
+	} 
+
+}
---
+++ Entregavel1/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author RafaOliveira
+ *
+ */
+module Entregavel1 {
+}
