26889b736d3d20d3deab1f453f747c5e9bcfd7bf
Rafael Oliveira <rafasdoliveira@outlook.com>
2022-11-27 18:30:51 -0300

    ordenacao done

--- Entregavel1/src/algortimosjava/ordenacao.java
+++ Entregavel1/src/algortimosjava/ordenacao.java
@@ -1,5 +1,29 @@
 package algortimosjava;
 
-public class ordenacao {
+import java.util.Scanner;
 
+public class ordenacao {
+	
+	public static void main(String[] args) {
+		
+		try (Scanner n = new Scanner(System.in)) {
+			System.out.print("Informe a quantidade de notas");
+			
+			int qtd = n.nextInt();
+			int nota;
+			int contador = 0;
+			int i = 0;
+			
+			while(i<qtd) {
+				System.out.print("Informe uma nota");
+				nota = n.nextInt();
+				if(nota<5) {
+					contador= contador+1;
+				}
+				i=i+1;
+			}
+			System.out.print("Quantidade de alunos aprovados: " + contador);
+		}
+	}
+	
 }
