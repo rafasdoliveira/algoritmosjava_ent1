129bb58dd3fbcd324c05b73eed1a0bb7fdb11b4f
Rafael Oliveira <rafasdoliveira@outlook.com>
2022-11-26 23:07:33 -0300

    somatorio code

--- Entregavel1/src/algortimosjava/somatorio.java
+++ Entregavel1/src/algortimosjava/somatorio.java
@@ -1,10 +1,16 @@
 package algortimosjava;
 
+import java.util.Scanner;
+
 public class somatorio {
 	
 	public static void main(String[] args) {
 		
-		System.out.print("Ola, mundo!");
+		Scanner num = new Scanner (System.in);
+		int a = num.nextInt();
+		int b = num.nextInt();
+		int soma = a + b;
+		System.out.println("A o seu resultado eh = " +soma);
 	} 
 
 }
