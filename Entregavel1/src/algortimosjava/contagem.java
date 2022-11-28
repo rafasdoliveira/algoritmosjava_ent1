427663b09223a5ae23e5773313cebde0816deb03
Rafael Oliveira <rafasdoliveira@outlook.com>
2022-11-27 23:38:24 -0300

    contagem done

--- Entregavel1/src/algortimosjava/contagem.java
+++ Entregavel1/src/algortimosjava/contagem.java
@@ -1,5 +1,29 @@
 package algortimosjava;
 
-public class contagem {
+import java.util.Scanner;
 
+public class contagem {
+	
+	public static void main(String[] args) {
+	
+		try(Scanner n = new Scanner(System.in)) {
+			System.out.println("Informe a quantidades de notas: ");
+            int num = n.nextInt();
+            int nota;
+            int contador = 0;
+            int i = 0;
+            
+            while(i<num){
+                System.out.println("Informe uma nota");
+                nota = n.nextInt();
+                if(nota>=5){
+                    contador = contador+1;
+            }
+            i = i+1;
+   }
+   System.out.println("Quantidade de alunos aprovados no teste: " + contador);
+
+		}
+	}
 }
+
