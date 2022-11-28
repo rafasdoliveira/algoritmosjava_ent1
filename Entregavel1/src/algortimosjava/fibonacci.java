9807bc57cd8741dcdf87c18dd6d82604d65947a1
Rafael Oliveira <rafasdoliveira@outlook.com>
2022-11-27 21:01:43 -0300

    fibonacci done

--- Entregavel1/src/algortimosjava/fibonacci.java
+++ Entregavel1/src/algortimosjava/fibonacci.java
@@ -1,5 +1,26 @@
 package algortimosjava;
 
-public class fibonacci {
+import java.util.Scanner;
 
+public class fibonacci {
+	
+	public static void main(String[] args) {
+		
+		try(Scanner n = new Scanner(System.in)) {
+			System.out.println("Informe o número de termos da sequência: ");
+			
+			int num = n.nextInt();
+		    int termo1= 0;
+		    int termo2= 1;
+		    int termo3;
+		    System.out.println(termo1+", "+termo2);
+		    
+		    for(int i=3;i<=num;i++){
+		    termo3=termo1+termo2;
+		    System.out.println(", "+termo3);
+		    termo1=termo2;
+		    termo2=termo3;
+		    }
+		}
+	}
 }
